f81d077e5058b5cff0af4b79831eff52f7e6f9de
iselad <100822135+iselad@users.noreply.github.com>
2022-03-02 23:42:20 -0600

    Update Form1.vb
    cambio

--- WinFormsApp1/WinFormsApp1/Form1.vb
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -2,7 +2,7 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
         MessageBox.Show("Hola mundo")
-
+        MessageBox.Show("quiero queso")
 
 
     End Sub
